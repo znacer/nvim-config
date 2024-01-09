2c78df2f0c76330f7798bf079330145e07ded000
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-01-09 22:53:01 +0100

    telescope configuration

--- lua/plugins/main.lua
+++ lua/plugins/main.lua
@@ -7,4 +7,38 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function(_, opts)
+      table.insert(opts.sections.lualine_x, "😄")
+    end,
+  },
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+      {
+        "<leader>fd",
+        function()
+          require("telescope.builtin").lsp_definitions({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Definition",
+      },
+      {
+        "<leader>fh",
+        function()
+          require("telescope.builtin").lsp_type_definitions({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Type Definition",
+      },
+    },
+  },
 }
