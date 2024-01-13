45f4cc7170e988db3de71bd906db082592a78fc0
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-01-13 15:09:19 +0100

    first rice

---
+++ lua/plugins/jupyter.lua
@@ -0,0 +1,6 @@
+-- TODO: install and configure https://github.com/benlubas/molten-nvim
+-- looks like a powerfull tool for interacting with jupyter notebooks using neovim
+
+if true then
+  return {}
+end
