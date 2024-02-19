131ad17da20622bd36781ec5600dce1336297253
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-02-19 12:29:48 +0100

    rust

--- lua/plugins/jupyter.lua
+++
@@ -1,6 +0,0 @@
--- TODO: install and configure https://github.com/benlubas/molten-nvim
--- looks like a powerfull tool for interacting with jupyter notebooks using neovim
-
-if true then
-  return {}
-end
