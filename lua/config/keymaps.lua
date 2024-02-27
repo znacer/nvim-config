f78ccc382d7d41a01338029a80bbfefdd48e91df
Zakaria Nacer <zak.nacer@hotmail.fr>
2024-02-27 17:16:18 +0100

    kickstart inspired

--- lua/config/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
-local map = vim.keymap.set
--- remap scrolling
-map("n", "<C-d>", "<C-d>zz", { desc = "Move down" })
-map("n", "<C-u>", "<C-u>zz", { desc = "Move down" })
